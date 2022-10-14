117044f19b2e6e900de3992d1eac27d69414d660
Andrey Lapin <nethack@mail.ru>
2022-10-14 15:46:52 +0400

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,6 +15,37 @@ const { NotImplementedError } = require("../extensions/index.js");
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
+function getInnerStr(option) {
+  if (typeof option.addition !== "string" && option.addition !== undefined) {
+    option.addition = String(option.addition);
+  }
+
+  if (!option.addition) {
+    return "";
+  }
+
+  let separator = "|";
+  let repeatTimes = 1;
+
+  if (option.additionSeparator) {
+    separator = option.additionSeparator;
+  }
+
+  if (option.additionRepeatTimes) {
+    repeatTimes = option.additionRepeatTimes;
+  }
+
+  let additionArr = [];
+
+  for (let i = 0; i < repeatTimes; i++) {
+    additionArr.push(option.addition);
+  }
+
+  let result = additionArr.join(separator);
+
+  return result;
+}
+
 function repeater(str, option) {
   const {
     repeatTimes,
@@ -23,13 +54,31 @@ function repeater(str, option) {
     additionRepeatTimes,
     additionSeparator,
   } = option;
-  const arr = Array(Number(repeatTimes));
 
-  arr.reduce((acc, _, i) => {
-    acc += str
-    acc += separator
-  }, '')
+  const innerString = getInnerStr(option);
+
+  let separatorDefault = "+";
+  let repeat = 1;
+
+  if (separator) {
+    separatorDefault = separator;
+  }
+
+  if (repeatTimes) {
+    repeat = repeatTimes;
+  }
+
+  let strArr = [];
+
+  for (let i = 0; i < repeat; i++) {
+    strArr.push(String(str) + innerString);
+  }
+
+  const result = strArr.join(separatorDefault);
+  return result;
 }
+
+console.log(repeater("la", { repeatTimes: 3 }));
 
 module.exports = {
   repeater,
